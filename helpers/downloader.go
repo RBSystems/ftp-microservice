ed7a385b0a4ef09cc9b30a9581f29bb8b456f5b7
Jesse Millar <hellojessemillar@gmail.com>
2016-06-01 14:55:02 -0600

    Don't redownload files if we already have a copy

--- helpers/downloader.go
+++ helpers/downloader.go
@@ -12,24 +12,26 @@ func DownloadFile(request Request) (Request, error) {
 	urlTokens := strings.Split(request.FileLocation, "/")
 	request.Filename = urlTokens[len(urlTokens)-1]
 
-	// Download the referenced file
-	output, err := os.Create("downloads/" + request.Filename)
-	if err != nil {
-		return Request{}, err
-	}
+	if _, err := os.Stat("downloads/" + request.Filename); os.IsNotExist(err) { // Download the file if it doesn't exist
+		// Download the referenced file
+		output, err := os.Create("downloads/" + request.Filename)
+		if err != nil {
+			return Request{}, err
+		}
 
-	defer output.Close()
+		defer output.Close()
 
-	response, err := http.Get(request.FileLocation)
-	if err != nil {
-		return Request{}, err
-	}
+		response, err := http.Get(request.FileLocation)
+		if err != nil {
+			return Request{}, err
+		}
 
-	defer response.Body.Close()
+		defer response.Body.Close()
 
-	_, err = io.Copy(output, response.Body)
-	if err != nil {
-		return Request{}, err
+		_, err = io.Copy(output, response.Body)
+		if err != nil {
+			return Request{}, err
+		}
 	}
 
 	SendFile(request) // Start sending the file
