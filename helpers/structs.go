b2d811a538f0bc1ffdfef570f4d9dcb83dcb4689
Jesse Millar <hellojessemillar@gmail.com>
2016-07-07 15:56:44 -0600

    Misunderstood my own code and told Dan something that was a lie about the fileLocation token

--- helpers/structs.go
+++ helpers/structs.go
@@ -7,9 +7,8 @@ type Request struct {
 	// Required fields
 	DestinationAddress   string `json:"destinationAddress"`   // Hostname to be sent the file
 	DestinationDirectory string `json:"destinationDirectory"` // Path indicates where to store the file on the server
-	FileLocation         string `json:"fileLocation"`         // Local location of the file to be sent (must be accessible from the server running the service)
-	Filename             string `json:"filename"`
-	CallbackAddress      string `json:"callbackAddress"` // Complete address to send the notification that the file transfer is complete
+	FileLocation         string `json:"fileLocation"`         // The remote URL of the file to be downloaded and sent
+	CallbackAddress      string `json:"callbackAddress"`      // Complete address to send the notification that the file transfer is complete
 
 	// Optional Fields
 	CallbackIdentifier string `json:"callbackIdentifier,omitempty"` // Optional value to be passed in so the requester can identify the host when it's sent back
@@ -18,6 +17,7 @@ type Request struct {
 	PasswordFTP        string `json:"passwordFTP,omitempty"`        // Password to authenticate with the device (defaults to anonymous)
 
 	// Fields not expected in request, will be filled by the service
+	Filename       string
 	SubmissionTime time.Time // Will be filled by the service to indicate when the file transfer began
 	CompletionTime time.Time // Will be filled by the service to indicate when the file transfer ended or timed out
 	Status         string    // "Timeout" or "Success"
