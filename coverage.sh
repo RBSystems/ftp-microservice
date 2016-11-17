4832ba8db98f35ed482f10a1552e9f2adea03bc8
Jesse Millar <hellojessemillar@gmail.com>
2016-11-17 10:00:12 -0700

    Setting up Glide dependencies

--- coverage.sh
+++ coverage.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
+# This script gathers Go test results for uploading to codecov.io
+
 set -e
 echo "" > coverage.txt
 
-for d in $(find ./* -maxdepth 10 -type d); do
-    if ls $d/*.go &> /dev/null; then
-        go test -coverprofile=profile.out -covermode=atomic $d
-        if [ -f profile.out ]; then
-            cat profile.out >> coverage.txt
-            rm profile.out
-        fi
-    fi
+for d in $(find ./* -maxdepth 10 -type d -not -path "*vendor*"); do
+	if ls $d/*.go &> /dev/null; then
+		go test -coverprofile=profile.out -covermode=atomic $d
+		if [ -f profile.out ]; then
+			cat profile.out >> coverage.txt
+			rm profile.out
+		fi
+	fi
 done
